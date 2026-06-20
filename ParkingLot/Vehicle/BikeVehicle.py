45a87e74bbec2ebf9a37264865179e1999e70685
pratyaksh sharma <pratyakshsharma40@gmail.com>
2026-06-20 17:33:29 +0530

    builder

--- ParkingLot/Vehicle/BikeVehicle.py
+++
@@ -1,4 +0,0 @@
-from .Vehicle import Vehicle
-class BikeVehicle(Vehicle):
-    def __init__(self,vehicle_type,licence_plate,fee_strategy):
-        super().__init__(vehicle_type,licence_plate,fee_strategy)
--- ParkingLot/Vehicle/TruckVehicle.py
+++
@@ -1,4 +0,0 @@
-from .Vehicle import Vehicle
-class TruckVehicle(Vehicle):
-    def __init__(self, vechicle_type, licence_plate, fee_stategy):
-        super().__init__(vechicle_type, licence_plate, fee_stategy)
